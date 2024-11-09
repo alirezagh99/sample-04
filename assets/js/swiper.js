af0bcaad0959cc338b5ce761ed15d876971c3891
Alireza Ghasemi <agh102724@gmail.com>
2024-11-09 13:53:42 +0330

    add-new-arrivals

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -42,3 +42,77 @@ const swiper = new Swiper(".swiper", {
   //     el: ".swiper-scrollbar",
   //   },
 });
+
+// const swiper2 = new Swiper(".swiper2", {
+//   // Optional parameters
+//   direction: "horizontal",
+//   loop: true,
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   // If we need pagination
+//   //   pagination: {
+//   //     el: ".swiper-pagination",
+//   //     clickable: true,
+//   //   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next2",
+//     prevEl: ".swiper-button-prev2",
+//   },
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 20,
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 1,
+//       spaceBetween: 30,
+//     },
+//     // when window width is >= 640px
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 40,
+//     },
+//     900: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//     },
+//   },
+
+//   // And if we need scrollbar
+//   //   scrollbar: {
+//   //     el: ".swiper-scrollbar",
+//   //   },
+// });
+
+var swiper2 = new Swiper(".swiper2", {
+  pagination: ".swiper-pagination2",
+  paginationClickable: true,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
